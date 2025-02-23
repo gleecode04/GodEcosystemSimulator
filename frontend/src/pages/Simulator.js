5f5c33f16650c95627c2acf776aea30c19de0bd3
jojobaest <joiewqyeung@gmail.com>
2025-02-22 23:35:34 -0500

    set up simulator containers

--- frontend/src/pages/Simulator.js
+++ frontend/src/pages/Simulator.js
@@ -1,10 +1,17 @@
 import React from "react";
+import "./Simulator.css";
 
 const Simulator = () => {
   return (
     <div className="simulation-container">
-      <h1>Simulation Page</h1>
-      <p>This is where the interactive simulation will be.</p>
+      <div className="simulation-main">
+        <h2>Simulation View</h2>
+        {/* Main simulation content will go here */}
+      </div>
+      <div className="simulation-controls">
+        <h2>Controls</h2>
+        {/* Control panel content will go here */}
+      </div>
     </div>
   );
 };
